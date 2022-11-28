05153808940c5ddf4fb12129c9a3313375642d3e
Test Student <cuteststudent@billkuker.com>
2022-11-28 08:12:17 -0500

    class prep

---
+++ src/art/homework.js
@@ -0,0 +1,38 @@
+import { makeNoise3D } from "fast-simplex-noise";
+import { hsl, gray } from "../color.js";
+
+let n = makeNoise3D();
+
+/**
+ * I call this shade function once for every pixel in the image, and 
+ * set the color equal to whatever you return.
+ * 
+ * I call it 30 times a second so you can animate things
+ * 
+ * @param x - The x position in the image, from 0 (left) to 1 (right)
+ * @param y - The y position in the image, from 0 (top) to 1 (bottom)
+ * @param t - The time passed, in seconds
+ * @returns The RGB color to display at the x,y location at time t
+ */
+function shade(x, y, t) {
+    //❓❓ Question 2
+    //❓❓ Question 3
+    //❓❓ Question 4
+    return [1,0,1];
+}
+
+/**
+ * I call this draw function 30 times per second.
+ * @param ctx - The 2d drawing context
+ * @param t -The time in seconds
+ */
+function draw(ctx, t) {
+    //See https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'black';
+    //❓❓ Question 5
+    //❓❓ Question 6
+    ctx.strokeRect(75, 100, 150, 100);
+}
+
+export default { name: "My Homework", shade, draw }
--- src/main.js
+++ src/main.js
@@ -1,47 +1,83 @@
+////////////////////////////////////////////////////////////////
+//This first section is concerned with loading each "art"
+// and letting the user select which one to show.
+////////////////////////////////////////////////////////////////
+
+//Load each art
 import meta from "./art/metaballs.js";
 import fire from "./art/fire.js";
 import fractal from "./art/fractal.js";
 import sunset from "./art/sunset.js";
+import homework from "./art/homework.js";
 
-let choices = [
+//Put them in an array
+let arts = [
+    homework,
     meta,
     fire,
     fractal,
     sunset
 ];
-let current = choices[0];
+//And set the first one as the current art to show
+let currentArt = arts[0];
 
+//Find the select box
 let select = document.querySelector("#select");
-choices.forEach( (c,i) => {
-    select.appendChild(new Option(c.name, i, i) );
+//Fill in the choices
+arts.forEach((c, i) => {
+    select.appendChild(new Option(c.name, i, i));
 });
+//Add an event listener...
 select.addEventListener('change', e => {
-    current = choices[select.value];
+    //When the selection changes to a new number, make that
+    //art the current art to show.
+    currentArt = arts[select.value];
 });
 
-let size = 256;
-let app = document.querySelector("#app");
-let c = document.querySelector("#canvas");
 
-var ctx = c.getContext("2d");
-let d = ctx.createImageData(size, size);
+////////////////////////////////////////////////////////////////
+//This section sets up to display the graphics
+////////////////////////////////////////////////////////////////
 
+const size = 256;
+const app = document.querySelector("#app");
+const c = document.querySelector("#canvas");
+
+const ctx = c.getContext("2d");
+const imageData = ctx.createImageData(size, size);
+
+//Save the time we started
 let start = new Date().getTime();
+
+//This function draws one frame
 function frame() {
+    //How much time has passed since we started?
     let now = new Date().getTime();
     let t = now - start;
+
+    //Execute the art's "Shade" function
+
+    //For each pixel in the canvas...
     for (let x = 0; x < size; x++) {
         for (let y = 0; y < size; y++) {
-            let c = current.shade(x / size, y / size, t / 1000);
+            //Get the pixel for that location
+            let c = currentArt.shade(x / size, y / size, t / 1000);
+
+            //And put it in the correct place in the image data array
             let i = 4 * (size * y + x);
-            d.data[i + 0] = c[0] * 255; //R
-            d.data[i + 1] = c[1] * 255; //G
-            d.data[i + 2] = c[2] * 255; //B
-            d.data[i + 3] = 255;        //A
+            imageData.data[i + 0] = c[0] * 255; //R
+            imageData.data[i + 1] = c[1] * 255; //G
+            imageData.data[i + 2] = c[2] * 255; //B
+            imageData.data[i + 3] = 255;        //A
         }
     }
-    ctx.putImageData(d, 0, 0);
-    current.draw(ctx, t);
+    //Draw it to the canvas
+    ctx.putImageData(imageData, 0, 0);
+
+    //Execute the art's "Draw" function
+    currentArt.draw(ctx, t / 1000);
+
+    //Please call this function again soon
     window.requestAnimationFrame(frame);
 }
 window.requestAnimationFrame(frame);
