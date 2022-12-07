91090f1b1fdfbbc86b89889a9440f2b7d5255919
rxt06130 <rxt06130@castleton.edu>
2022-12-07 11:55:43 -0500

    art!

--- src/art/homework.js
+++ src/art/homework.js
@@ -15,10 +15,11 @@ let n = makeNoise3D();
  * @returns The RGB color to display at the x,y location at time t
  */
 function shade(x, y, t) {
-    //❓❓ Question 2
-    //❓❓ Question 3
-    //❓❓ Question 4
-    return [x, y, Math.sin(t)];
+  return [0,0,y];
+    // return [
+     //   Math.sin(t*10 + 20*x),
+       // Math.cos(t*10 + 20*y),
+        //0];
 }
 
 /**
@@ -28,11 +29,38 @@ function shade(x, y, t) {
  */
 function draw(ctx, t) {
     //See https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = '#F0E68C';
+    ctx.beginPath(130, 135);
+ctx.moveTo(130, 40);
+ctx.lineTo(40, 40);
+ctx.lineTo(110, 120);
+ctx.lineTo(80,10);
+ctx.lineTo(30, 115);
+ctx.closePath();
+ctx.stroke();
+
+
+ctx.beginPath(230, 240);
+ctx.moveTo(230, 140);
+ctx.lineTo(140, 140);
+ctx.lineTo(210, 220);
+ctx.lineTo(180,110);
+ctx.lineTo(130, 215);
+ctx.closePath();
+ctx.stroke();
+ {
+    ctx.beginPath();
+    ctx.arc(200, 45, 20, 10, 100 * Math.PI);
+    ctx.fillStyle = '#FFFACD';
+    ctx.fill();
+}
+
+
     //❓❓ Question 5
     //❓❓ Question 6
-    ctx.strokeRect(75, 100, 150 + 10 * Math.sin(t), 100 + 10 * Math.cos(t)); 
+   //ctx.strokeRect(75, 100, 150 + 10 * Math.sin(t), 100 + 10 * Math.cos(t)); 
 }
 
 export default { name: "My Homework", shade, draw }
+
